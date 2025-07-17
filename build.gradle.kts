05a29391a9ae6c21bd9ed1b1a27a686136e34f3b
Daemon Jung <jjjssswww1006@gmail.com>
2025-07-18 02:56:40 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val koinVersion: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.2.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.insert-koin:koin-ktor:${koinVersion}")
+    implementation("io.insert-koin:koin-logger-slf4j:${koinVersion}")
+}
