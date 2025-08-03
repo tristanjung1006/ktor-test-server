9ec65af848e150d434f7c8b08aa57b82e20d1f45
Daemon Jung <jjjssswww1006@gmail.com>
2025-08-03 21:20:48 +0900

    staticResources about images package

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -3,11 +3,14 @@ package com.example.plugins
 import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+
+        staticResources("/images", "images")
     }
 }
