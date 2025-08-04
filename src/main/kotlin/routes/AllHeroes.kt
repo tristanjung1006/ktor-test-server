5383b0ed7e289d0619681af00de72c965c3144c3
Daemon Jung <jjjssswww1006@gmail.com>
2025-08-04 15:13:31 +0900

    heroRepository koin injection

--- src/main/kotlin/routes/AllHeroes.kt
+++ src/main/kotlin/routes/AllHeroes.kt
@@ -1,19 +1,29 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.response.respond
+import io.ktor.server.routing.application
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by application.inject()
+
     get("/boruto/heroes") {
         // 클라이언트가 엔드포인트에서 오류를 발생하면 엘비스 연산자로 기본 1페이지 제공
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
 
-            call.respond(message = page)
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Only Numbers Allowed"),
