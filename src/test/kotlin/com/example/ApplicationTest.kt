39baf42f4dd8a2d213b1f1a0d664f2c43f2caf68
Daemon Jung <jjjssswww1006@gmail.com>
2025-08-04 17:01:00 +0900

    test code base

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun `access root endpoint, assert correct information`() =
+        testApplication {
+            application { module() }
+            val response = client.get("/")
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = response.status
+            )
+            assertEquals(
+                expected = "Welcome to Boruto API!",
+                actual = response.bodyAsText()
+            )
+        }
+}
